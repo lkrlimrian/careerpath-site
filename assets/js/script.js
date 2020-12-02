938aac50a3b4a00d20512c79ef243462af6bd885
lkrlimrian <lkrlimrian@gmail.com>
2020-12-02 17:22:38 +0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$('.role').click(function(){
+    $(this).addClass('active');
+    $(this).parent().next().removeClass('disabled');
+
+    if($(":selected", this)){
+        console.log($(this).attr('id'))
+    }
+})
+
+$('.reset').click(function(){
+    $('.level-group').addClass('disabled');
+    $('.level-0, .level-1').removeClass('disabled');
+    $(':checked').prop("checked", false);
+})
