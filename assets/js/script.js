5cb1cc6cd04621626bf55d1b293ccbed79b29076
lkrlimrian <lkrlimrian@gmail.com>
2020-12-06 16:44:42 +0800

    previous role interactions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,12 @@
 var imgpath = 'assets/img/';
 
-
-$('.role').on('click', function(){
-    $(this).addClass('active');
-    $(this).parent().next().removeClass('disabled');
-
-    if( $(this).parent().hasClass('level-1')){
-        $('button.reset').css('opacity', '1');
-        $('.ttstart').css({
-            'opacity': 0,
-            'z-index': -9999
-        })
-    }
-
-    if($(":selected", this)){
-        console.log($(this).attr('id'))
-        $(this).find('.role-img').attr('src', imgpath + $(this).attr('id') + '1.png');
-    }
-    else{
-        $(this).find('.role-img').attr('src', imgpath + $(this).attr('id') + '.png');
-    }
-
-    $(this).parent('.level-group').find('.active').removeClass('active');
-    $(this).addClass('active');
-})
-
 $('.opspath').click(function(){
     $('.qapath').each(function(){
         $(this).find('.role-img').attr('src', imgpath + $(this).attr('id') + '.png');
         $(this).removeClass('active')
     })
     $(this).parent('.level-group').prevAll('.level-group').find('.opspath').click();
-})
+});
 
 $('.qapath').click(function(){
     $('.opspath').each(function(){
@@ -69,4 +44,40 @@ $('.reset').click(function(){
 
     // REMOVE ALL ACTIVE PATHS
     $('.active').removeClass('active');
-})
+});
+
+// PREV ROLE AND DISABLE NEXT
+$('.level-group').click(function(){
+    $(this).nextAll('.level-group').addClass('disabled');
+    $(this).next('.level-group').removeClass('disabled');
+    $(this).nextAll('.level-group').find(':checked').prop("checked", false);
+    $(this).nextAll('.level-group').find('.role').each(function(){
+        var r = $(this);
+        r.find('.role-img').attr('src', imgpath + r.attr('id') + '.png');
+        r.removeClass('active');
+    });
+});
+
+$('.role').on('click', function(){
+    $(this).addClass('active');
+    $(this).parent().next().removeClass('disabled');
+
+    if( $(this).parent().hasClass('level-1')){
+        $('button.reset').css('opacity', '1');
+        $('.ttstart').css({
+            'opacity': 0,
+            'z-index': -9999
+        })
+    }
+
+    if($(":selected", this)){
+        console.log($(this).attr('id'))
+        $(this).find('.role-img').attr('src', imgpath + $(this).attr('id') + '1.png');
+    }
+    else{
+        $(this).find('.role-img').attr('src', imgpath + $(this).attr('id') + '.png');
+    }
+
+    $(this).parent('.level-group').find('.active').removeClass('active');
+    $(this).addClass('active');
+});
